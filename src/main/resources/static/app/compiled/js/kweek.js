24b3c744102b3a4bbc85f802ea37f5b1f1f3a2d6
Harrison Favour <fayvorharrison101@gmail.com>
2017-06-02 16:31:31 +0100

    started adding functionality for google location api

--- src/main/resources/static/app/compiled/js/kweek.js
+++ src/main/resources/static/app/compiled/js/kweek.js
@@ -38160,70 +38160,117 @@ angular.module('ui.router.state')
     ];
 };
 
+/*var loadMap = function () {
+    var mapOptions = {
+        center: new google.maps.LatLng(17.240498, 82.287598),
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: map.getCenter(),
+        map: map,
+        animation: google.maps.Animation.BOUNCE
+    });
+    var infoWindow = new google.maps.InfoWindow({
+        content: "current location"
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+        infoWindow.open(map, marker);
+    });
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			marker.setPosition(pos);
+			map.setCenter(pos);
+		}, function(){
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else{
+		handleLocationError(false, infoWindow, map.getCenter);
+	}
+};
+function handleLocationError(browserHasGeoLocation, infoWindow, pos){
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeoLocation ? "Error: The GeoLocation service failed." : "Error: Your browser does not support geolocation");
+	infoWindow.open(map);
+};
+
+};*/
+
 
 var app = angular.module('kweek', ['ngCookies', 'ui.router']);
 
 app.config(['$httpProvider', '$cookiesProvider', '$stateProvider', '$urlRouterProvider',
-                function ($httpProvider, $cookiesProvider, $stateProvider, $urlRouterProvider) {
+    function ($httpProvider, $cookiesProvider, $stateProvider, $urlRouterProvider) {
 
-    $httpProvider.defaults.headers.common['Accept'] = "application/json";
-    $httpProvider.defaults.headers.common['Content-Type'] = "application/json";
-    $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common['Accept'] = "application/json";
+        $httpProvider.defaults.headers.common['Content-Type'] = "application/json";
+        $httpProvider.defaults.useXDomain = true;
 
-    $cookiesProvider.defaults.path = "/";
-    $cookiesProvider.defaults.secure = true;
-    // $cookiesProvider.defaults.expires = fill in a date;
+        $cookiesProvider.defaults.path = "/";
+        $cookiesProvider.defaults.secure = true;
+        // $cookiesProvider.defaults.expires = fill in a date;
 
-    $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
 
-    $stateProvider
-        .state('home', {
-            url: '/',
-            controller: 'MainController',
-            templateUrl: '/app/home.html'
-        })
-        .state('view-car-suv', {
-            url: '/vehicles/suv',
-            controller: 'VehicleController',
-            templateUrl: '/app/modules/vehicle/view-suv.html'
-        })
-        .state('view-car-all-terrain', {
-            url: '/vehicles/all-terrain',
-            controller: 'VehicleController',
-            templateUrl: '/app/modules/vehicle/view-vehicles.html'
-        })
-        .state('view-car-salon', {
-            url: '/vehicles/salon',
-            controller: 'VehicleController',
-            templateUrl: '/app/modules/vehicle/view-salon.html'
-        })
-        .state('new-vehicle', {
-            url: '/vehicle/new-vehicle',
-            controller: 'VehicleController',
-            templateUrl: '/app/modules/vehicle/new-vehicle.html'
-        })
-        .state('view-vehicles', {
-            url: '/vehicle/all-vehicles',
-            controller: 'VehicleController',
-            templateUrl: '/app/modules/vehicle/view-vehicles.html'
-        })
-        .state('new-reservation', {
-            url: '/reservation/new-reservation',
-            controller: 'ReservationController',
-            templateUrl: '/app/modules/reservation/new-reservation.html'
-        })
-        .state('all-reservations', {
-            url: '/reservation/all-reservations',
-            controller: 'ReservationController',
-            templateUrl: '/app/modules/reservation/view-reservations.html'
-        })
-        .state('user-profile', {
-            url: '/user/profile',
-            controller: 'UserController', 
-            templateUrl: '/app/modules/user/user-profile.html'
-        });
+        $stateProvider
+            /*.state('home', {
+                url: '/',
+                controller: 'MainController',
+                templateUrl: '/app/home.html'
+            })*/
+            .state('home', {
+                url: '/',
+                controller: 'VehicleController',
+                templateUrl: '/app/modules/vehicle/view-vehicles.html'
+            })
+            .state('view-car-suv', {
+                url: '/vehicles/suv',
+                controller: 'VehicleController',
+                templateUrl: '/app/modules/vehicle/view-suv.html'
+            })
+            .state('view-car-all-terrain', {
+                url: '/vehicles/all-terrain',
+                controller: 'VehicleController',
+                templateUrl: '/app/modules/vehicle/view-vehicles.html'
+            })
+            .state('view-car-salon', {
+                url: '/vehicles/salon',
+                controller: 'VehicleController',
+                templateUrl: '/app/modules/vehicle/view-salon.html'
+            })
+            .state('new-vehicle', {
+                url: '/vehicle/new-vehicle',
+                controller: 'VehicleController',
+                templateUrl: '/app/modules/vehicle/new-vehicle.html'
+            })
+            .state('view-vehicles', {
+                url: '/vehicle/all-vehicles',
+                controller: 'VehicleController',
+                templateUrl: '/app/modules/vehicle/view-vehicles.html'
+            })
+            .state('new-reservation', {
+                url: '/reservation/new-reservation',
+                controller: 'ReservationController',
+                templateUrl: '/app/modules/reservation/new-reservation.html'
+            })
+            .state('all-reservations', {
+                url: '/reservation/all-reservations',
+                controller: 'ReservationController',
+                templateUrl: '/app/modules/reservation/view-reservations.html'
+            })
+            .state('user-profile', {
+                url: '/user/profile',
+                controller: 'UserController',
+                templateUrl: '/app/modules/user/user-profile.html'
+            });
 
-}]);;app.service('APIService', ['$http', '$q', function ($http, $q) {
+    }]);;app.service('APIService', ['$http', '$q', function ($http, $q) {
 
     this.get = function (url, successHandler, errorHandler) {
         $http.get(url)
